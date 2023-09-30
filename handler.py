3160c8e0119772e86113a1bd9df57c9b18214020
feiyang <935410746@qq.com>
2023-09-30 21:13:35 +0800

    1

--- handler.py
+++ handler.py
@@ -1,48 +1,37 @@
-from boto3 import client as boto3_client
+# from boto3 import client as boto3_client
+import boto3
 
 input_bucket = "inputbucket77"
-s3 = boto3_client('s3', region_name='us-east-1')
-
-# 清空bucket
-def clear_bucket(bucketName):
-	# global input_bucket
-	# s3 = boto3_client('s3', region_name='ap-northeast-2')
-	list_obj = s3.list_objects_v2(Bucket=bucketName)
-	try:
-		for item in list_obj["Contents"]:
-			key = item["Key"]
-			s3.delete_object(Bucket=bucketName, Key=key)
-	except:
-		print("Nothing to clear in bucket: " + bucketName)
+dynamodb_name = "studentInfor"
+s3 = boto3.client('s3', region_name='us-east-1')
 
 def lambda_handler(event, context):
     print("Hello3")
     bucket_name = event['Records'][0]['s3']['bucket']['name']
+    # key就是文件名
     key = event['Records'][0]['s3']['object']['key']
 
     try:
         print("bucket_name: "+ bucket_name)
         print("key: "+ key)
-        # file_name = getFileNameFromS3(bucket_name, key)
-        # print('FileName:', file_name)
-        # deleteFileFromS3(bucket_name, key)
+        # 删除文件
         s3.delete_object(Bucket=bucket_name, Key=key)
-        print("Hello3.8")
+        print("Hello3.1")
     except Exception as e:
         print('Error:', str(e))
 
-def getFileNameFromS3(bucket_name, key):
-    print("Hello3.2")
-    s3_client = boto3.client('s3')
-    print("Hello3.3")
-    response = s3_client.head_object(Bucket=bucket_name, Key=key)
-    print("Hello3.4")
-    print("getFileNameFromS3: "+response['Metadata'])
-    # return response['Metadata']['filename']
+# 从
+def get_item_from_dynamodb(key):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(dynamodb_name)
+    response = table.get_item(Key=key)
+    item = response.get('Item')
+    
+    return item
 
-def deleteFileFromS3(bucket_name, key):
-    print("Hello3.5")
-    s3_client = boto3.client('s3')
-    print("Hello3.6")
-    s3_client.delete_object(Bucket=bucket_name, Key=key)
-    print("Hello3.7")
+# 使用示例
+key = {
+    'name': 'floki'
+}
+item = get_item_from_dynamodb(key)
+print(item)
